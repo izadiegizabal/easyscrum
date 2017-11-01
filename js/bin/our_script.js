a6759f8594934b375415faa4e30e080488ad73b6
khoabui9 <khoabui.vn@gmail.com>
2017-11-01 18:16:21 +0200

    scrumboard backend

--- js/bin/our_script.js
+++ js/bin/our_script.js
@@ -6,6 +6,87 @@ $( document ).ready(function() {
     // Init selects
     $('select').select();
 
+    $('.open_modal').click(function() {
+        var id = $(this).attr('id');
+        $('.save_modal').attr("id", id);
+    });
+
+    $('.save_modal').click(function() {
+        var id = $(this).attr('id');
+        var title = $('.modal-title').val();
+        var des = $('.modal-description').val();
+        var reas = $('.modal-reason').val();
+        var es = $('.modal-estimate').val();
+        var val = $('.modal-value').val();
+
+         $.ajax({
+            method: 'POST',
+            url: 'create.php?' + 'id=' + id + '&title=' + title + '&description=' + des + '&reason=' + reas + '&estimate=' + es + '&val=' + val,
+            data: 'id=' + id + '&title=' + title + '&description=' + des + '&reason=' + reas + '&estimate=' + es + '&val=' + val,
+            success: function () {
+                console.log(this.url);
+                
+            },
+            error: function () {
+            }
+        })
+    });
+
+    var draggableOptions = {
+        cancel: "a.ui-icon",
+        revert: true,
+        cursor: "move",
+        revertDuration: 0
+    }
+    $('.card-task').draggable(draggableOptions, {
+        drag: function () {
+            $(this).css("z-index", "1");
+        }
+    });
+
+    $('.fc-event-container').draggable({
+    revert: true,      // immediately snap back to original position
+    revertDuration: 0  //
+    });
+
+    $('.mycard').droppable();
+
+     var codes = {
+        "1": "#1",
+        "2": "#2",
+        "3": "#3",
+        "4": "#4",
+        "5": "#5",
+
+    }
+    $.each(codes, function (index, value) {
+        $('.mycard').droppable({
+            drop: function (event, ui) {
+                var element = ui.draggable;
+                $(this).append(element);
+                var id = $(this).attr('id');
+                $(element).children().attr('id', id);
+                $(element).css("z-index", "0");
+                var task_id = $(element).attr('id');
+                var card_id = $(this).parent().attr('id');
+                $.ajax({
+                    method: 'POST',
+                    url: 'update_ref.php?' + 'id=' + task_id + '&cardId=' + card_id,
+                    cache: false,
+                    data: 'id=' + task_id + '&cardId=' + card_id,
+                    success: function () {
+                        console.log('success');
+                    },
+                    error: function () {
+                    }
+                });
+            }
+        });
+    });
+
+
+
+
 /*
     // CHECKLIST
     // Add a new item
--- src/scrumboard/scrumboards.php
+++ src/scrumboard/scrumboards.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
     <!--Import Google Icon Font-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="../../sass/materialize.css"  media="screen,projection"/>
-
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
@@ -16,71 +18,91 @@
 
     <div class="row progress-col">
         <!--Product Backlog-->
-        <div class="col s12 m4 l2">
+        <div id='1' class="col s12 m4 l2">
             <div class="pink backlog-header">
                 <h5 class="white-text center">Product Backlog</h5>
             </div>
-            <div class="grey lighten-3 backlog-body">
+            <div class="grey lighten-3 backlog-body mycard" style="min-height: 400px;">
                 <div class="">
                     <!-- Modal Trigger -->
-                    <a class="waves-effect waves-light btn modal-trigger pink" href="#modal1">+</a>
+                    <a id='1' class="open_modal waves-effect waves-light btn modal-trigger pink" href="#modal1">+</a>
                 </div>
+                <?php
+                    $card_id = 1;
+                    include 'show.php';
+                ?>
             </div>
         </div>
         <!--End Product Backlog-->
 
         <!--Sprint Backlog-->
-        <div class="col s12 m4 l2">
+        <div id='2' class="col s12 m4 l2">
             <div class="purple backlog-header">
                 <h5 class="white-text center">Product Backlog</h5>
             </div>
-            <div class="grey lighten-3 backlog-body">
+            <div class="grey lighten-3 backlog-body mycard" style="min-height: 400px;">
                 <div class="">
                     <!-- Modal Trigger -->
-                    <a class="waves-effect waves-light btn modal-trigger purple" href="#modal1">+</a>
+                    <a id='2' class="open_modal waves-effect waves-light btn modal-trigger purple" href="#modal1">+</a>
                 </div>
+                <?php
+                    $card_id = 2;
+                    include 'show.php';
+                ?>
             </div>
         </div>
         <!--End Sprint Backlog-->
 
         <!--In Progress-->
-        <div class="col s12 m4 l2">
+        <div id='3' class="col s12 m4 l2">
             <div class="deep-purple backlog-header">
                 <h5 class="white-text center">In Progress</h5>
             </div>
-            <div class="grey lighten-3 backlog-body">
+            <div class="grey lighten-3 backlog-body mycard" style="min-height: 400px;">
                 <div class="">
                     <!-- Modal Trigger -->
-                    <a class="waves-effect waves-light btn modal-trigger deep-purple" href="#modal1">+</a>
+                    <a id='3' class="open_modal waves-effect waves-light btn modal-trigger deep-purple" href="#modal1">+</a>
                 </div>
+                <?php
+                    $card_id = 3;
+                    include 'show.php';
+                ?>
             </div>
         </div>
         <!--End In Progress-->
 
         <!--Done Sprint-->
-        <div class="col s12 m4 l2">
+        <div id='4' class="col s12 m4 l2">
             <div class="indigo backlog-header">
                 <h5 class="white-text center">Done Sprint</h5>
             </div>
-            <div class="grey lighten-3 backlog-body">
+            <div class="grey lighten-3 backlog-body mycard" style="min-height: 400px;">
                 <div class="">
                     <!-- Modal Trigger -->
-                    <a class="waves-effect waves-light btn modal-trigger indigo" href="#modal1">+</a>
+                    <a id='4' class="open_modal waves-effect waves-light btn modal-trigger indigo" href="#modal1">+</a>
                 </div>
+                <?php
+                    $card_id = 4;
+                    include 'show.php';
+                ?>
             </div>
         </div>
         <!--End Done Sprint-->
 
         <!--Done Project-->
-        <div class="col s12 m4 l2">
+        <div id='5' class="col s12 m4 l2">
             <div class="blue backlog-header">
                 <h5 class="white-text center">Done Project</h5>
             </div>
-            <div class="grey lighten-3 backlog-body">
+            <div class="grey lighten-3 backlog-body mycard" style="min-height: 400px;">
                 <div class="">
                     <!-- Modal Trigger -->
-                    <a class="waves-effect waves-light btn modal-trigger blue" href="#modal1">+</a>
+                    <a id='5' class="open_modal waves-effect waves-light btn modal-trigger blue" href="#modal1">+</a>
                 </div>
+                <?php
+                    $card_id = 5;
+                    include 'show.php';
+                ?>
             </div>
         </div>
         <!--End Done Project-->
@@ -90,7 +112,7 @@
     <div id="modal1" class="modal">
         <div class="task-header">
             <div class="input-field white-text">
-                <input placeholder="PB1: Checklist..." id="task-title" type="text" class="white-text">
+                <input placeholder="PB1: Checklist..." id="task-title" type="text" class="modal-title white-text">
                 <label for="task-title">Task Title</label>
             </div>
             <div>
@@ -99,23 +121,23 @@
         </div>
         <div class="task-content">
             <h5>User Story</h5>
-            <div class="input-field">
-                <input placeholder="User" id="us-as" type="text">
+         <!--    <div class="input-field">
+                <input class="user-who" placeholder="User" id="us-as" type="text">
                 <label for="us-as">As a(n)</label>
-            </div>
+            </div> -->
             <div class="input-field">
-                <textarea  placeholder="Be able to create as much checklists I think are necessary for the project / case" id="us-want" class="materialize-textarea"></textarea>
+                <textarea  placeholder="Be able to create as much checklists I think are necessary for the project / case" id="us-want" class="modal-description materialize-textarea"></textarea>
                 <label for="us-want">I want to</label>
             </div>
             <div class="input-field">
-                <textarea  placeholder="I can create subtasks that I need to finish first before having finished the bigger ones" id="us-so" class="materialize-textarea"></textarea>
+                <textarea  placeholder="I can create subtasks that I need to finish first before having finished the bigger ones" id="us-so" class="modal-reason materialize-textarea"></textarea>
                 <label for="us-so">So that</label>
             </div>
         </div>
         <div class="task-footer">
             <div class="row">
                 <div class="input-field col s12 m6">
-                    <select>
+                    <select class="modal-estimate">
                         <option value="" disabled>Make a guess</option>
                         <option value="0">0</option>
                         <option value="0.5">0.5</option>
@@ -133,7 +155,7 @@
                     <label>Estimate</label>
                 </div>
                 <div class="input-field col s12 m6">
-                    <select>
+                    <select class="modal-value">
                         <option value="" disabled>Make a guess</option>
                         <option value="0">0</option>
                         <option value="0.5">0.5</option>
@@ -153,12 +175,12 @@
             </div>
             <div class="modal-footer">
                 <a href="#!" class="modal-action modal-close waves-effect waves-light btn-flat white-text">Cancel</a>
-                <a href="#!" class="modal-action modal-close waves-effect waves-light btn-flat white-text">Save</a>
+                <a href="#!" class="save_modal modal-action modal-close waves-effect waves-light btn-flat white-text">Save</a>
             </div>
         </div>
     </div>
     <!--JavaScript -->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
     <script type="text/javascript" src="../../js/bin/materialize.js"></script>
     <script type="text/javascript" src="../../js/bin/our_script.js"></script>
 </body>
